6d3cb3255134d128f4ae1f9e41c1a3d60dae16b9
uttam raj <uttamrajsali@gmail.com>
2013-10-08 11:40:36 -0700

    comit 10.8 with just grabbing names from xml file

--- cstr/src/main/java/org/yujie/oditest/cssr/ReadXMLFile.java
+++ cstr/src/main/java/org/yujie/oditest/cssr/ReadXMLFile.java
@@ -16,25 +16,22 @@ public class ReadXMLFile {
   public static void main(String args[]) {
  
     try {
-   	Login logintest = new Login();
-    logintest.execute();
+   	//Login logintest = new Login();
+    //logintest.execute();
     File fXmlFile = new File("staff.xml");
 	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 	Document doc = dBuilder.parse(fXmlFile);
-	Class cls = Class.forName("org.yujie.oditest.cssr.SelectTimeRange");
-	Object obj = cls.newInstance();
-	Class noparams[] = {};
-	//optional, but recommended
-	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	
 	doc.getDocumentElement().normalize();
  
-	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());//company
  
-	NodeList nList = doc.getElementsByTagName("may");
- 
+	NodeList nList = doc.getElementsByTagName("classname");
+	
 	System.out.println("----------------------------");
  
+	//This for loop execute only twice one for id = select time range another for id = timestep
 	for (int temp = 0; temp < nList.getLength(); temp++) {
  
 		Node nNode = nList.item(temp);
@@ -42,21 +39,41 @@ public class ReadXMLFile {
 		System.out.println("\nCurrent Element :" + nNode.getNodeName());
  
 		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			
+			/*NodeList tList = nNode.getChildNodes();
+			for(int i = 0; i <tList.getLength(); i++ ){
+				Node tNode = tList.item(i);
+			*/
  
 			Element eElement = (Element) nNode;
- 
-			System.out.println("Staff id : " + eElement.getAttribute("id"));
-			//System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
-			//System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
-			//System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
-			//System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+			NodeList tLists = eElement.getElementsByTagName("testname");
+			for (int j = 0; j < tLists.getLength(); j++)
+		    {
+		        Element x = (Element) tLists.item(j);
+			System.out.println(x.getFirstChild().getNodeValue());
+			//System.out.println(tNode.getNodeName());
+			
+		//	System.out.println(" Name : " + eElement.getElementsByTagName("testname").item(0).getTextContent());
+			//System.out.println(" Name 2: " + eElement.getElementsByTagName("testname").item(0).getTextContent());
+			}
+			/*String ame = eElement.getAttribute("id");
+			Class cls = Class.forName(ame);
+			Object obj = cls.newInstance();
+			Class noparams[] = {};
+			
+			/*System.out.println("Staff id : " + eElement.getAttribute("id"));
+			System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+			System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
 			System.out.println("Testname : " + eElement.getElementsByTagName("testname").item(0).getTextContent());
 			String x = eElement.getElementsByTagName("testname").item(0).getTextContent();
+			*/
 			//Method method = getClass().getMethod(x);
 			//method.invoke(this);
-			System.out.println(x);
-			Method method = cls.getDeclaredMethod(x, noparams);
-			method.invoke(obj, null);
+			//System.out.println(x);
+			//Method method = cls.getDeclaredMethod(x, noparams);
+			//method.invoke(obj, null);
 			
 			
 		}
