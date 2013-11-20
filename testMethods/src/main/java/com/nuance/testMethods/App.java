0d5e296cfe56fc5f390b811c7b8f8b4ecec86e17
uttam raj <uttamrajsali@gmail.com>
2013-11-20 14:32:46 -0800

    before pull2

---
+++ testMethods/src/main/java/com/nuance/testMethods/App.java
@@ -0,0 +1,19 @@
+package com.nuance.testMethods;
+
+import org.nuance.testFrame.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ReadXMLFile xmlRead= new ReadXMLFile();
+        xmlRead.readXml("testcases.xml");
+        GenerateHtml FinalRep = new GenerateHtml();
+        FinalRep.execute();
+    }
+}
